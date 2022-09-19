94da3e262e5935529a9dd434a14d611a59cf3c52
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-09-19 09:48:41 -0500

    Initial Commit

---
+++ BatteryLevelsDemo/BatteryLevelsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryLevelsDemoApp.swift
+//  BatteryLevelsDemo
+//
+//  Created by Mark Volkmann on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct BatteryLevelsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
